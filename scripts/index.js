66438f0463eb5ea02027f43e425cb6b7fb925d1a
Pedro <pedropequera@gmail.com>
2022-10-30 12:51:27 -0300

    Entrega Post

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import initHome from "../pages/home/home.js";
-import initPage from "../pages/post/post.js";
+import initPage from "../post.js";
 import initCategoryScroll from "./categoryScroll.js";
 import initSmoothScroll from "./smoothScroll.js";
 
